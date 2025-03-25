241da917b41303973891c25f2a6a47c142a37bf3
송경호 <s07019@naver.com>
2025-03-25 10:35:17 +0900

    refactor: refactor program viewer

---
+++ src/content/components/Error.tsx
@@ -0,0 +1,19 @@
+import { Frown } from "lucide-react";
+
+const Error = () => {
+  return (
+    <div className="flex flex-auto items-center justify-center">
+      <div className="flex flex-row items-center gap-3">
+        <Frown className="h-7 w-7" />
+        <div className="m-0 flex flex-col justify-start p-0">
+          <p className="m-0 p-0 text-lg">Sorry something went wrong</p>
+          <p className="m-0 p-0 text-sm">
+            Help us improve ECMA Visualizer by reporting the issue
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/content/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { LoaderCircle } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="absolute left-0 top-0 z-[900] flex size-full items-center justify-center">
+      <LoaderCircle className="h-10 w-10 animate-spin font-bold text-[#E79118]" />
+    </div>
+  );
+};
+
+export default Loading;
