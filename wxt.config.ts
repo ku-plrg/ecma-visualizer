2a4d14afcb989327dda39a2b1fe3d955584d65aa
Minseok Choe <choems@outlook.com>
2025-04-17 05:49:48 +0900

    chore: project setting

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,10 @@
+import path from "path";
 import { defineConfig } from 'wxt';
+import react from "@vitejs/plugin-react";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
   manifest: {
     name: "ECMA Visualizer",
     description: "providing example programs for ECMAScript Language Specification",
@@ -12,26 +15,50 @@ export default defineConfig({
         "16": "images/dimmed-icon-16.png"
       }
     },
-    background: {
-      "service_worker": "background.js"
-    },
-    permissions: ["storage", "tabs", "activeTab"],
-    content_scripts: [
-      {
-        js: ["content.js"],
-        css: ["content.css"],
-        matches: [
-          "https://tc39.es/ecma262/2024/*",
-          "https://262.ecma-international.org/15.0/*"
-        ]
-      }
-    ],
+    // background: {
+    //   "service_worker": "background/index.ts"
+    // },
+    permissions: ["storage", "tabs", "activeTab", "sidePanel"],
+    // content_scripts: [
+    //   {
+    //     js: ["content/index.tsx"],
+    //     css: ["index.css"],
+    //     matches: [
+    //       "https://tc39.es/ecma262/2024/*",
+    //       "https://262.ecma-international.org/15.0/*"
+    //     ]
+    //   }
+    // ],
     web_accessible_resources: [
       {
         resources: ["images/logo.jpeg", "resources/*"],
         matches: ["https://tc39.es/*", "https://262.ecma-international.org/*"]
       }
     ]
-  }
-  
+  },
+  // https://vite.dev/config/
+  vite: () => ({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "entrypoints"),
+      }
+    },
+    build: {
+      cssCodeSplit: true,
+      // rollupOptions: {
+      //   input: {
+      //     content: "./src/content/index.tsx",
+      //     background: "./src/background/index.ts",
+      //   },
+      //   output: {
+      //     entryFileNames: "[name].js",
+      //     assetFileNames: "content.css",
+      //   },
+      // },
+    },
+  })
 });
+
+
+
