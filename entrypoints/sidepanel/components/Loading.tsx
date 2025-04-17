91d06e7a5e68b856825e60922c57f328529afabd
Minseok Choe <choems@outlook.com>
2025-04-17 13:42:57 +0900

    style: use relative

--- entrypoints/sidepanel/components/Loading.tsx
+++ entrypoints/sidepanel/components/Loading.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from "lucide-react";
 
 export const Loading = () => {
   return (
-    <div className="absolute left-0 top-0 z-[900] flex size-full items-center justify-center">
+    <div className="flex size-full items-center justify-center">
       <LoaderCircle className="h-10 w-10 animate-spin font-bold text-[#E79118]" />
     </div>
   );
