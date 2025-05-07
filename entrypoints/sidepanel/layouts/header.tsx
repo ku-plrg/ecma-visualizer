f12f6a4fecbded880d8815e8ffb7c6c4c9000d6e
Minseok Choe <choems@outlook.com>
2025-05-08 01:55:06 +0900

    revert

--- entrypoints/sidepanel/layouts/header.tsx
+++ entrypoints/sidepanel/layouts/header.tsx
@@ -1,32 +1,12 @@
 import { GitHubIcon } from "@/entrypoints/sidepanel/icons";
-import { QueryClient } from "@tanstack/query-core";
-import { useAtom } from "jotai";
-import { atomWithSuspenseQuery } from "jotai-tanstack-query";
-import { unwrap } from "jotai/utils";
 import { Suspense } from "react";
 
 const logo = browser.runtime.getURL("/images/logo.jpeg");
 
-const qcTest = new QueryClient()
-
-const xAtom = atomWithSuspenseQuery(
-  () => ({
-    queryKey: ["test"],
-    queryFn: async () => 1,
-  }),
-  () => qcTest
-)
-
-function Test() {
-  const [x, setX] = useAtom(unwrap(xAtom))
-  return <div><div>{x?.data}</div><button onClick={setX}>setX</button></div>
-
-}
-
 export function Header() {
   return (
     <header className="flex flex-row items-center justify-between px-2 py-2">
-      <div className="flex flex-row items-center justify-start text-sm gap-1">
+      <div className="flex flex-row items-center justify-start gap-1 text-sm">
         <a
           href={import.meta.env.VITE_ESMETA_URL}
           target="_blank"
